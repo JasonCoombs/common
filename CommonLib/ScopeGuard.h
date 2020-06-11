f1f110f46f1a6198728662ead4996e77a9db5871
Pavel Kokolemin <pavelkok@gmail.com>
2020-06-11 16:32:00 +0300

    Add ScopedGuard helper

---
+++ CommonLib/ScopeGuard.h
@@ -0,0 +1,35 @@
+/*
+
+***********************************************************************************
+* Copyright (C) 2020 - 2020, BlockSettle AB
+* Distributed under the GNU Affero General Public License (AGPL v3)
+* See LICENSE or http://www.gnu.org/licenses/agpl.html
+*
+**********************************************************************************
+
+*/
+
+#ifndef SCOPED_GUARD_H
+#define SCOPED_GUARD_H
+
+#include <functional>
+
+class ScopedGuard
+{
+public:
+   explicit ScopedGuard(std::function<void()> f)
+      : f_(std::move(f))
+   {
+   }
+
+   ~ScopedGuard()
+   {
+      f_();
+   }
+
+private:
+   std::function<void()> f_;
+
+};
+
+#endif
