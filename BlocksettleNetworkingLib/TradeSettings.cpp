ada723a787dd0f9ef08efcb8e12145f966c18bc5
Pavel Kokolemin <pavelkok@gmail.com>
2020-06-24 12:07:45 +0300

    Send trade settings to terminal

---
+++ BlocksettleNetworkingLib/TradeSettings.cpp
@@ -0,0 +1,31 @@
+/*
+
+***********************************************************************************
+* Copyright (C) 2020 - 2020, BlockSettle AB
+* Distributed under the GNU Affero General Public License (AGPL v3)
+* See LICENSE or http://www.gnu.org/licenses/agpl.html
+*
+**********************************************************************************
+
+*/
+#include "TradeSettings.h"
+
+#include "bs_types.pb.h"
+
+using namespace bs;
+
+void TradeSettings::toPb(types::TradeSettings &msg)
+{
+   msg.set_xbt_tier1_limit(xbtTier1Limit);
+   msg.set_xbt_price_band(xbtPriceBand);
+   msg.set_auth_required_settled_trades(authRequiredSettledTrades);
+}
+
+TradeSettings TradeSettings::fromPb(const types::TradeSettings &msg)
+{
+   TradeSettings result;
+   result.xbtTier1Limit = msg.xbt_tier1_limit();
+   result.xbtPriceBand = msg.xbt_price_band();
+   result.authRequiredSettledTrades = msg.auth_required_settled_trades();
+   return result;
+}
