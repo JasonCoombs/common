14e74e42bbb5467de2d3a75e4ee99ca6c1698678
Ation <gation@gmail.com>
2020-08-17 18:37:08 +0300

    Add uat pub key

--- BlocksettleNetworkingLib/ApplicationSettings.cpp
+++ BlocksettleNetworkingLib/ApplicationSettings.cpp
@@ -784,7 +784,7 @@ std::string ApplicationSettings::GetBlocksettlePublicKey() const
       case ApplicationSettings::EnvConfiguration::Production:
          return "";
       case ApplicationSettings::EnvConfiguration::Test:
-         return "";
+         return "0355621c63cb9b3e2449cceb7a0fbbaf37b9d123fe6bc6e465c4dc3ebcca36df41";
 #ifndef PRODUCTION_BUILD
       case ApplicationSettings::EnvConfiguration::Staging:
          return "0332834fa4d4bef48d651573a23a8257b981f3000e592d22f270fffdf5c66a3b18";
