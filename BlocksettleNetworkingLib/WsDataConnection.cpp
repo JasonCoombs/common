201643885224c828b8be582cd7f65aa1340f536c
Pavel Kokolemin <pavelkok@gmail.com>
2020-10-01 17:27:01 +0300

    Detect failed AEAD on client side

--- BlocksettleNetworkingLib/WsDataConnection.cpp
+++ BlocksettleNetworkingLib/WsDataConnection.cpp
@@ -147,6 +147,20 @@ bool WsDataConnection::isActive() const
    return context_ != nullptr;
 }
 
+bool WsDataConnection::timer(std::chrono::milliseconds timeout, DataConnection::TimerCallback callback)
+{
+   if (!isActive()) {
+      SPDLOG_LOGGER_ERROR(logger_, "can't start timer because connection is not active");
+      return false;
+   }
+   if (listenThread_.get_id() != std::this_thread::get_id()) {
+      SPDLOG_LOGGER_ERROR(logger_, "starting timer from non-listening thread is not supported");
+      return false;
+   }
+   timers_.scheduleCallback(context_, timeout, std::move(callback));
+   return true;
+}
+
 int WsDataConnection::callbackHelper(lws *wsi, int reason, void *user, void *in, size_t len)
 {
    auto context = lws_get_context(wsi);
