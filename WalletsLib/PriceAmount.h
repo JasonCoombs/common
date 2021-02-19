bea9eea49779e9bbe2c19992a6681358c3cef484
Ation <gation@gmail.com>
2021-02-19 12:53:26 +0200

    Add dealing spread update messages

--- WalletsLib/PriceAmount.h
+++ WalletsLib/PriceAmount.h
@@ -24,28 +24,39 @@ namespace bs {
    {
    public:
       using Self = PriceAmount<precision>;
+      using underlying_type = int64_t;
 
       PriceAmount()
-         : value_{}
+         : value_{0}
       {
       }
 
       // Amount is truncated toward zero, so PriceAmount<2>(1.1299) is equal to PriceAmount<2>(1.12)
-      PriceAmount(double amount)
+      explicit PriceAmount(double amount)
       {
-         value_ = static_cast<int64_t>(std::trunc(amount * scale()));
+         value_ = static_cast<underlying_type>(std::trunc(amount * scale()));
       }
+
+      PriceAmount(const Self&) = default;
+      Self& operator = (const Self&) = default;
+
+      PriceAmount(Self&&) = default;
+      Self& operator = (Self&&) = default;
 
       std::string to_string() const
       {
          std::ostringstream ss;
-         int64_t base = std::abs(value_) / scale();
-         int64_t rem = std::abs(value_) % scale();
+         underlying_type base = std::abs(value_) / scale();
+         underlying_type rem = std::abs(value_) % scale();
          if (value_ < 0) {
             ss << '-';
          }
          ss << base << "." << std::setw(precision) << std::setfill('0') << rem;
          return ss.str();
+      }
+
+      bool isZero() const {
+         return value_ == 0;
       }
 
       double to_double() const
@@ -79,20 +90,33 @@ namespace bs {
          return PriceAmount(value_ - other.value_);
       }
 
+      underlying_type serialize() const
+      {
+         return value_;
+      }
+
+      static Self Deserialize(underlying_type data)
+      {
+         return Self{data};
+      }
+
    private:
-      static constexpr uint32_t scaleHelper(uint32_t value)
+      static constexpr underlying_type scaleHelper(underlying_type value)
       {
           return value == 0 ? 1 : 10 * scaleHelper(value - 1);
       }
 
-      static constexpr uint32_t scale()
+      static constexpr underlying_type scale()
       {
          return scaleHelper(precision);
       }
 
-      explicit PriceAmount(int64_t amount);
+      explicit PriceAmount(underlying_type amount)
+      {
+         value_ = amount;
+      }
 
-      int64_t value_;
+      underlying_type value_;
    };
 
    using CentAmount = PriceAmount<2>;
