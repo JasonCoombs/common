7c0531863f4ed8d784b0b2b5b790699223fdb5d5
Sergey Chernikov <s.bilberry@gmail.com>
2021-11-18 16:30:21 +0300

    updated build scripts for Auth eID server

--- build_scripts/curl_settings.py
+++ build_scripts/curl_settings.py
@@ -1,7 +1,7 @@
 #
 #
 # ***********************************************************************************
-# * Copyright (C) 2018 - 2020, BlockSettle AB
+# * Copyright (C) 2018 - 2021, BlockSettle AB
 # * Distributed under the GNU Affero General Public License (AGPL v3)
 # * See LICENSE or http://www.gnu.org/licenses/agpl.html
 # *
@@ -19,8 +19,8 @@ from component_configurator import Configurator
 class CurlSettings(Configurator):
     def __init__(self, settings):
         Configurator.__init__(self, settings)
-        self._version = '7_63_0'
-        self._script_revision = '3'
+        self._version = '7_80_0'
+        self._script_revision = '4'
         self._package_name = 'curl-' + self._version
         self._package_url = 'https://github.com/curl/curl/archive/' + self._package_name + '.tar.gz'
         self._package_dir_name = 'curl-' + self._package_name
@@ -48,6 +48,7 @@ class CurlSettings(Configurator):
         command = ['cmake',
             '-G',
             self._project_settings.get_cmake_generator(),
+            '-A x64 '
             '-DCURL_DISABLE_FTP=ON',
             '-DCURL_DISABLE_LDAP=ON',
             '-DCURL_DISABLE_LDAPS=ON',
@@ -67,54 +68,22 @@ class CurlSettings(Configurator):
             '-DBUILD_TESTING=OFF',
         ]
 
-        result = subprocess.call(command)
+        if self._project_settings.on_windows():
+            cmdStr = r' '.join(command)
+            result = subprocess.call(cmdStr)
+        else:
+            result = subprocess.call(command)
         return result == 0
 
     def get_solution_file(self):
         return os.path.join(self.get_build_dir(), 'CURL.sln')
 
-    def make_windows(self):
-        print('Making curl: might take a while')
-        command = ['devenv',
-                   self.get_solution_file(),
-                   '/build',
-                   self.get_win_build_mode(),
-                   '/project', 'curl']
-
+    def make(self):
+        command = ['cmake', '--build', '.']
         result = subprocess.call(command)
         return result == 0
 
-    def get_win_build_mode(self):
-        if self._project_settings.get_build_mode() == 'release':
-            return 'RelWithDebInfo'
-        else:
-            return 'Debug'
-
-    def make_x(self):
-        command = ['make', '-j', str(multiprocessing.cpu_count())]
-
+    def install(self):
+        command = ['cmake', '--build', '.', '--target', 'install']
         result = subprocess.call(command)
         return result == 0
-
-    def install_win(self):
-        # copy libs
-        output_dir = os.path.join(self.get_install_dir(), 'lib')
-        lib_dir = os.path.join(self.get_build_dir(), 'lib', self.get_win_build_mode())
-        print('copy from ' + lib_dir + ' to ' + output_dir)
-        self.filter_copy(lib_dir, output_dir, '.lib')
-
-        # copy headers
-        output_dir = os.path.join(self.get_install_dir(), 'include')
-        inc_dir = os.path.join(self.get_build_dir(), 'include')
-        self.filter_copy(inc_dir, output_dir, '.h')
-
-        return True
-
-    def install_x(self):
-        command = ['make', 'install']
-        result = subprocess.call(command)
-        if result != 0:
-            print('Failed to install Curl')
-            return False
-
-        return True
--- build_scripts/sqlite_settings.py
+++ build_scripts/sqlite_settings.py
@@ -1,7 +1,7 @@
 #
 #
 # ***********************************************************************************
-# * Copyright (C) 2018 - 2020, BlockSettle AB
+# * Copyright (C) 2018 - 2021, BlockSettle AB
 # * Distributed under the GNU Affero General Public License (AGPL v3)
 # * See LICENSE or http://www.gnu.org/licenses/agpl.html
 # *
@@ -18,8 +18,10 @@ from component_configurator import Configurator
 class SqliteSettings(Configurator):
     def __init__(self, settings):
         Configurator.__init__(self, settings)
-        self._package_name = "sqlite-amalgamation-3200100"
-        self._package_url = "https://www.sqlite.org/2017/" + self._package_name + ".zip"
+        self._version = '3360000'
+        self._script_revision = '1'
+        self._package_name = "sqlite-amalgamation-" + self._version
+        self._package_url = "https://www.sqlite.org/2021/" + self._package_name + ".zip"
 
     def get_package_name(self):
         return self._package_name
@@ -44,4 +46,17 @@ class SqliteSettings(Configurator):
 
     def install(self):
         self.filter_copy(self.get_unpacked_sources_dir(), self.get_install_dir())
+        cwd = os.getcwd()
+        os.chdir(self.get_install_dir())
+
+        env_vars = os.environ.copy()
+
+        if self._project_settings.on_windows():
+           subprocess.call(["cl", "sqlite3.c", "/c"], env=env_vars)
+           subprocess.call(["lib", "sqlite3.obj"], env=env_vars)
+        else:
+           subprocess.call(["cc", "-c", "-o", "sqlite3.o", "sqlite3.c"], env=env_vars)
+           subprocess.call(["ar", "rcs", "libsqlite3.a", "sqlite3.o"], env=env_vars)
+
+        os.chdir(cwd)
         return True
