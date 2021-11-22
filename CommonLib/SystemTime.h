bbcb096efe31538ab2152dd8f5878420c4490e7c
Ation <gation@gmail.com>
2021-11-22 19:06:17 +0200

    Minor changes

--- CommonLib/SystemTime.h
+++ CommonLib/SystemTime.h
@@ -9,6 +9,12 @@ namespace sytem_time
    using time_point_t = system_clock_t::time_point;
 
    uint64_t getTimestampUTC();
+
+   template<class D>
+   uint64_t getDurationMS(const D& duration)
+   {
+      return std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+   }
 }
 
 #endif // __SYSTEM_TIME_H__
